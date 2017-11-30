040891a97ca25f3e2cfcfd279c12e7d046ead7f6
Ross Dowthwaite <ross.dowthwaite@theunit.co.uk>
2017-11-30 09:42:32 +0000

    build script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,10 +1,16 @@
 import gulp from 'gulp';
 import browserify from "browserify";
 import source from "vinyl-source-stream";
+import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import strip from "gulp-strip-comments";
+import rename from "gulp-rename";
 import fs from 'fs';
 
 const destPaths = {
-  js: './dist/scripts'
+  root: './dist',
+  js: './dist/assets/scripts',
+  img: './dist/assets/img'
 }
 
 const sourcePaths = {
@@ -24,7 +30,30 @@ gulp.task('browserify', () => {
   .pipe(gulp.dest( destPaths.js ));
 });
 
-
 gulp.task('serve', () => {
   gulp.watch( sourcePaths.js,  [ 'browserify' ] );
+})
+
+gulp.task('build', ['scripts', 'vendorjs', 'images', 'files']);
+
+gulp.task('scripts', () => {
+  return gulp.src( ['./assets/scripts/*.js'])
+    .pipe( concat( 'scripts.js' ) )
+    .pipe( gulp.dest( destPaths.js ) )
+});
+
+gulp.task('vendorjs', () => {
+  return gulp.src( ['node_modules/aframe/dist/aframe.js'])
+    .pipe( concat( 'vendor.js' ) )
+    .pipe( gulp.dest( destPaths.js ) )
+})
+
+gulp.task('images', () => {
+  return gulp.src( './assets/img/*' )
+    .pipe( gulp.dest( destPaths.img ) )
+})
+
+gulp.task('files', () => {
+  return gulp.src( 'index.html' )
+    .pipe( gulp.dest( destPaths.root ) )
 })
